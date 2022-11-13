a96559e808d1f294e58d12bd55837f87ae0b88bf
egoistic_desire <kfkfjqjq@mail.ru>
2022-11-13 18:45:15 +0400

    secret_key is back

--- haba_haba/haba_haba/settings.py
+++ haba_haba/haba_haba/settings.py
@@ -10,28 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import environ
+# import environ
 from pathlib import Path
 
 from haba_haba.ckeditor_configs import CUSTOM_CONFIGS
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+# env = environ.Env(
+#     DEBUG=(bool, False)
+# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(BASE_DIR / '.env')
+# environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ww03tsn*%*s40!@pn=v$nxu4oxho$up$#udq8srn8d&@6p_xhs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
