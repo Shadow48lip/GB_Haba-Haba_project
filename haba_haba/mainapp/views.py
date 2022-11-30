48ed3752ac42d20998e2604e30557cdec5c5df1a
serg_myst <serg_myst@mail.ru>
2022-11-30 19:51:11 +0300

    haba_fill_db: описание по наполнению БД в README
    Для наполнения БД:
    fill_db
    картинки к статьям mainapp\fixtures\photos\
    
    модель HabaUser:
    добавлено поле slug
    удалено поле name
    
    Стартовая страница добавлен вывод статей

--- haba_haba/mainapp/views.py
+++ haba_haba/mainapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from django.views.generic import ListView, DetailView, CreateView, FormView
+from mainapp.models import Post
 
 
-# Заглушка
-def index(request):
-    return HttpResponse('НАЧИНАЕМ HABA-HABA')
+class MainappHome(ListView):
+    model = Post
+    template_name = 'mainapp/index.html'
+    context_object_name = 'posts'
+    allow_empty = False  # Будет генерироваться ошибка, если записей в таблице нет.
+    # Если мы вручную в строке браузера напишем не существующий путь
 
 
 # Обработчик не найденной страницы
--- haba_haba/userapp/models.py
+++ haba_haba/userapp/models.py
@@ -16,12 +16,12 @@ class HabaUser(AbstractUser):
 
     email = models.EmailField(null=False, unique=True, db_index=True, verbose_name='Электронная почта')
     creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    name = models.CharField(max_length=255, verbose_name="ФИО")
     age = models.PositiveSmallIntegerField(default=0, null=False)
     lock_date = models.DateField(verbose_name='Заблокирован до', default=datetime.date(2000, 1, 1))
     is_blocked = models.BooleanField(default=False, verbose_name='Заблокирован')
-    about = models.TextField(verbose_name="О себе")
+    about = models.TextField(verbose_name="О себе", blank=True)
     gender = models.CharField(verbose_name='Пол', choices=GENDER_CHOICES, blank=True, max_length=5)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='url')
 
     def __str__(self):
         return f'{self.username} / {self.age}'
