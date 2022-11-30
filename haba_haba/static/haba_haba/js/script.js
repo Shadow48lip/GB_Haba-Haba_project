3d5c93159742cb1bb2dfbf3863af4838625b501f
egoistic_desire <kfkfjqjq@mail.ru>
2022-11-30 23:46:36 +0400

    добавлено: страница для модератора, календарь

--- haba_haba/static/haba_haba/js/script.js
+++ haba_haba/static/haba_haba/js/script.js
@@ -48,3 +48,23 @@ if (iconMenu) {
     });
 }
 
+
+// календарь
+$(function () {
+    $('input[name="datetimes"]').daterangepicker({
+        timePicker: true,
+        timePickerIncrement: 5,
+        timePicker24Hour: true,
+        showDropdowns: true,
+        startDate: moment().startOf('hour'),
+        endDate: moment().startOf('hour').add(24, 'hour'),
+        locale: {
+            format: 'DD/MM/YYYY HH:mm',
+            daysOfWeek: ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'],
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            firstDay: 0,
+            applyLabel: 'Принять',
+            cancelLabel: 'Отмена',
+        }
+    });
+});
