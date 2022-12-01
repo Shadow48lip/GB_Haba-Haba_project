3ef8de63c805d16bfde3fa7a0660785482bf5f1b
Лариса <larolimp@gmail.com>
2022-12-01 11:56:53 +0300

    update setting.py secret key

--- haba_haba/haba_haba/settings.py
+++ haba_haba/haba_haba/settings.py
@@ -10,19 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-# import environ
+import environ
 from pathlib import Path
 
 from haba_haba.ckeditor_configs import CUSTOM_CONFIGS
 
-# env = environ.Env(
-#     DEBUG=(bool, False)
-# )
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
