dbe112ebdc27a2591a0e1e7e9e9d4088c69de2b7
egoistic_desire <kfkfjqjq@mail.ru>
2022-12-04 02:38:08 +0400

    добавлено: отображение пользователей в админке

--- haba_haba/userapp/admin.py
+++ haba_haba/userapp/admin.py
@@ -1,4 +1,39 @@
 from django.contrib import admin
 from .models import HabaUser
 
-admin.site.register(HabaUser)
+
+class HabaUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'slug',
+        'email',
+        'creation_time',
+        'is_active',
+        'is_blocked',
+        'is_staff',
+    )
+
+    list_display_links = (
+        'id',
+        'username',
+    )
+
+    search_fields = (
+        'username',
+    )
+    list_editable = (
+        'is_active',
+        'is_blocked',
+    )
+    list_filter = (
+        'is_active',
+        'is_blocked',
+        'creation_time',
+    )
+    prepopulated_fields = {
+        'slug': ('username',)
+    }
+
+
+admin.site.register(HabaUser, HabaUserAdmin)
