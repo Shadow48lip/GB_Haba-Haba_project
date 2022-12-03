04aed101f02a090309f052d8dd35b406c77f4efe
Sh2 Pro <shome@lipsat.ru>
2022-12-03 21:14:24 +0300

    Превью поста загружается из единого модуля. Добавил ссылку на профиль автора статьи. Пагинация статей в кабинете пользователя.

--- haba_haba/mainapp/templatetags/maintags.py
+++ haba_haba/mainapp/templatetags/maintags.py
@@ -1,24 +1,16 @@
 from django import template
-from django.template.defaultfilters import stringfilter
 
 from mainapp.models import Category
 
 register = template.Library()
 
 
-@register.filter(name='post_photo_process')
-@stringfilter
-def post_photo_process(string):
-    """
-    Если нет фото, то вставляет заглушку. В будущем можно добавить еще проверки.
-    """
-    if not string:
-        string = 'https://img.freepik.com/free-vector/tiny-characters-sitting-laptop-with-lorem-ipsum-title_74855-20389.jpg?w=1480&t=st=1665865814~exp=1665866414~hmac=0dfaf49fe1350925106e58bf01a6d184411e0f59751204fabe0edcd6c2ffda63'
-
-    return f'{string}'
-
-
-@register.inclusion_tag('mainapp/includes/site_main_categories.html')
+@register.inclusion_tag('mainapp/includes/site_main_categories.html', name='main_categories')
 def show_main_categories(cat_selected=None):
+    """Меню с главными категориями сайта.
+    @cat_selected: slug выбранной категории для выделения в меню.
+    """
     categories = Category.objects.all()
     return {'categories': categories, 'cat_selected': cat_selected}
+
+
--- haba_haba/userapp/urls.py
+++ haba_haba/userapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'user'
 urlpatterns = [
     path('', redirect_2_profile, name='profile_redirect'),
     path('profile/', my_profile_view, name='my_profile_view'),
-    path('profile/edit', my_profile_edit, name='my_profile_edit'),
-    path('profile/<int:pk>', UserProfile.as_view(), name='user_profile'),
+    path('profile/edit/', my_profile_edit, name='my_profile_edit'),
+    path('profile/<slug:slug>/', UserProfile.as_view(), name='user_profile'),
 ]
 
