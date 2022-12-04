4aadd0936351d1d05bc264533beecb3b5e54f817
Sh2 Pro <shome@lipsat.ru>
2022-12-04 16:36:00 +0300

    update

--- haba_haba/userapp/views.py
+++ haba_haba/userapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 from django.core.paginator import Paginator
 
 from .models import HabaUser
