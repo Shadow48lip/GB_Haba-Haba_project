fae668af5e24206d7416ed94a59127a604028855
Sh2 air <shome@lipsat.ru>
2022-12-08 13:44:49 +0300

    Кабинет пользователя теперь на CBV. В sidebar считаются счетчики

--- haba_haba/userapp/services.py
+++
@@ -1,9 +0,0 @@
-""" Служебные функции, которые могут понадобиться во многих местах. Бизнес логика. """
-from mainapp.models import Post
-
-
-def get_user_posts(user: object) -> Post:
-    """Запрос статей пользователя"""
-    # если надо запросить не все колонки values('title', 'content', 'slug')
-
-    return Post.objects.filter(author=user)
