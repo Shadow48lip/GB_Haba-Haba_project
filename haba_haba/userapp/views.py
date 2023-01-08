780fde2e47435d2edb6a795c1a1fb6129ae2f4fb
Sh2 Pro <shome@lipsat.ru>
2023-01-08 19:07:20 +0300

    fix sidebar на странице редактирования о себе

--- haba_haba/userapp/views.py
+++ haba_haba/userapp/views.py
@@ -50,6 +50,7 @@ class MyProfileUpdate(LoginRequiredMixin, DataMixin, UpdateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['object_user'] = self.request.user
         extra_context = self.get_user_context(user=self.request.user, title=f'Редактирование {self.request.user}')
 
         context = context | extra_context
