d867e6b7a2bbb11020e3e35744cecc45a56e0968
egoisticdesire <kfkfjqjq@mail.ru>
2023-01-08 17:00:56 +0400

    сортировка убрана и мелкие корректировки

--- haba_haba/mainapp/views.py
+++ haba_haba/mainapp/views.py
@@ -18,25 +18,25 @@ class MainappHome(DataMixin, PaginatorMixin, ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        order = self.request.GET.get('order_by', '-time_create')
+        # order = self.request.GET.get('order_by', '-time_create')
         return queryset.select_related(
             'author', 'cat'
-        ).filter(is_published=True, is_blocked=False).order_by(order)
+        ).filter(is_published=True, is_blocked=False) #.order_by(order)
 
     # def get_ordering(self):
     #     return self.request.GET.get('order_by')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        order_query = self.request.GET.get('order_by', '-time_create')
-
-        context['order_by'] = f"order_by={order_query}&"
+        # order_query = self.request.GET.get('order_by', '-time_create')
+        #
+        # context['order_by'] = f"order_by={order_query}&"
         extra_context = self.get_extra_context(title='Все категории')
         paginate_context = self.get_paginate_context()
         # оператор | объединяет словари (для python 3.9+)
         context = context | extra_context | paginate_context
 
-        print('main:\n', context)
+        # print('main:\n', context)
         return context
 
 
@@ -143,11 +143,11 @@ class PostCreateView(LoginRequiredMixin, DataMixin, CreateView):
     form_class = PostForm
     template_name = 'mainapp/page_post_create.html'
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.select_related(
-            'author', 'cat'
-        ).filter(is_published=True, is_blocked=False)  # .order_by('-time_create')
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     return queryset.select_related(
+    #         'author', 'cat'
+    #     ).filter(is_published=True, is_blocked=False)  # .order_by('-time_create')
 
     # Добавляем автора к публикации в момент сохранения
     def form_valid(self, form):
@@ -162,7 +162,7 @@ class PostCreateView(LoginRequiredMixin, DataMixin, CreateView):
 
         context = context | extra_context
 
-        # print('postcreate\n', context)
+        print('postcreate\n', context)
         return context
 
 
