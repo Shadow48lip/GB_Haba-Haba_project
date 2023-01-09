e2c0036ae12d18e17836130f877157878934010d
Sh2 air <shome@lipsat.ru>
2023-01-09 15:35:46 +0300

    убрал объяву про slug

--- haba_haba/moderatorapp/views.py
+++ haba_haba/moderatorapp/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 
 def moderator_index(request):
+    print(request.user.groups)
+    if request.user.is_superuser:
+        print('superuser')
     return HttpResponse('<h1>Модератор</h1>')
+
+# https://qna.habr.com/q/567186
