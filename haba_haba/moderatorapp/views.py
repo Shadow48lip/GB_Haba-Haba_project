fec21a33fa4d59a8bd65e6b94d72f66d620df4ea
Sh2 Pro <shome@lipsat.ru>
2023-01-09 18:47:01 +0300

    4 в модерильню пускаем только is_staff так же скрыта ссылка на модерку у обычных юзеров

--- haba_haba/moderatorapp/views.py
+++ haba_haba/moderatorapp/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
 
 def moderator_index(request):
-    print(request.user.groups)
-    if request.user.is_superuser:
-        print('superuser')
+    if not request.user.is_staff:
+        raise PermissionDenied()
+
     return HttpResponse('<h1>Модератор</h1>')
+
 
 
 # https://qna.habr.com/q/567186
